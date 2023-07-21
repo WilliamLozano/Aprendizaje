0e00129fe55d5bbe3f4ba6d82b7414919e85c809
William Lozano <lozanozalazarwilliamandres@gmail.com>
2023-07-21 16:33:57 -0500

    Ejercicios For hechos en clase

---
+++ Fundamentos de python/Ejercicios Estructuras de control/Eje33.py
@@ -0,0 +1,16 @@
+"""
+
+Partiendo de una frase imprimir palabra por palabra y un contador de palabras totales.
+
+"""
+
+    
+
+palabra = str(input("Ingrese una palabra : "))
+
+
+for i in palabra:
+    print(i)
+    
+
+print((f"La frase contiene {len(palabra)} letras"))
---
+++ Fundamentos de python/Ejercicios Estructuras de control/Eje34.py
@@ -0,0 +1,9 @@
+"""
+
+Imprimir una secuencia de caracteres que forme un patrón triangular como el siguiente:
+
+
+"""
+
+for i in range(1, 6): 
+    print("*" * i)
